32a468980b229ce30da2e65b6b7614ea15018827
donghank <donghank@student.42.fr>
2025-12-09 11:07:00 +0100

    overfiting found

--- predict.py
+++ predict.py
@@ -2,8 +2,13 @@ import numpy as np
 import pandas as pd
 import pickle	# To save and load the model (in serialized form)
 from multilayer_perceptron import MultilayerPerceptron
+from utils import cross_entropy
+import argparse
 
 def predict(data_path, model_path):
+	"""
+		Load the trained model and make predictions on the input data
+	"""
 	try:
 		with open(model_path, 'rb') as f:
 			model = pickle.load(f)
@@ -13,6 +18,11 @@ def predict(data_path, model_path):
 	
 	try:
 		df = pd.read_csv(data_path, header=None)
+
+		y_raw = df.iloc[:, 1].values
+
+		# Encode labels: 'M' -> 1, 'B' -> 0
+		y_true = np.array([1 if label == 'M' else 0 for label in y_raw])
 
 		X = df.iloc[:, 2:].values
 
@@ -24,6 +34,10 @@ def predict(data_path, model_path):
 	
 	probabilities = model.predict(X)
 
+	y_prob = probabilities[:, 1]
+
+	loss = cross_entropy(y_true, y_prob)
+
 	predictions = np.argmax(probabilities, axis=1)
 
 	labels = ["B", "M"]
@@ -32,5 +46,10 @@ def predict(data_path, model_path):
 	return decoded_predictions
 
 if __name__ == "__main__":
-	preds = predict("data.csv", "model.pkl")
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--data", default="data.csv", help="Path to the input data CSV file")
+	parser.add_argument("--model", default="model.pkl", help="Path to the trained model file")
+	args = parser.parse_args()
+
+	preds = predict(args.data, args.model)
 	print(preds)
