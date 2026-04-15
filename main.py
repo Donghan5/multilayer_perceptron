fix: main_train.py to main.py, fix predict.py to avoid data leakage

main.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import argparse
 import matplotlib.pyplot as plt
-from multilayer_perceptron import MultilayerPerceptron
+from utils import one_hot_encode
+
 
 def get_args():
 	parser = argparse.ArgumentParser(description="Train the MLP model.")
@@ -16,14 +17,7 @@ def get_args():
 	return parser.parse_args()
 
 
-def one_hot_encode(y):
-	encoded = np.zeros((len(y), 2))
-	for i, label in enumerate(y):
-		if label == 'M':
-			encoded[i] = [0, 1]
-		else:
-			encoded[i] = [1, 0]
-	return encoded
+
 
 def plot_learning_curve(history):
 	epochs = range(1, len(history['loss']) + 1)

network.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 
 from dataclasses import dataclass
-from utils import standarize, sigmoid, sigmoid_prime, relu, relu_prime, softmax, cross_entropy, cross_entropy_prime, mse, mse_prime
+from utils import standardize, sigmoid, sigmoid_prime, relu, relu_prime, softmax, cross_entropy, cross_entropy_prime, mse, mse_prime
 
 @dataclass
 class NetworkConfig:
 	layers: list[int]
 	activation: str
 	loss: str
-	output_activation: str # [FIX] Added field
+	output_activation: str
 
 class Network:
 	def __init__(self, config: NetworkConfig) -> None:
@@ -109,9 +109,9 @@ class Network:
 		nabla_b[-1] = np.sum(delta, axis=0)
 
 		for l in range(2, len(self.config.layers)):
-			current_activation = self.activations[-l]
+			z = self.zs[-l]
 
-			delta = np.dot(delta, self.weights[-l + 1].T) * self.activation_prime(current_activation)
+			delta = np.dot(delta, self.weights[-l + 1].T) * self.activation_prime(z)
 			nabla_w[-l] = np.dot(self.activations[-l - 1].T, delta)
 			nabla_b[-l] = np.sum(delta, axis=0)
 		

predict.py
@@ -1,42 +1,41 @@
 import numpy as np
 import pandas as pd
 import pickle	# To save and load the model (in serialized form)
-from multilayer_perceptron import MultilayerPerceptron
-from utils import cross_entropy
+import model
+from utils import cross_entropy, one_hot_encode
 import argparse
 
 def predict(data_path, model_path):
-	"""
-		Load the trained model and make predictions on the input data
-	"""
 	try:
 		with open(model_path, 'rb') as f:
 			model = pickle.load(f)
 	except FileNotFoundError:
 		print(f"Model file not found: {model_path}")
 		return
+
+	mean_train = model.mean_train
+	std_train = model.std_train
 	
+	# test data
 	try:
 		df = pd.read_csv(data_path, header=None)
 
 		y_raw = df.iloc[:, 1].values
-
-		# Encode labels: 'M' -> 1, 'B' -> 0
-		y_true = np.array([1 if label == 'M' else 0 for label in y_raw])
-
 		X = df.iloc[:, 2:].values
 
 		# Standardize features
-		X = (X - X.mean(axis=0)) / X.std(axis=0)
+		X = (X - mean_train) / std_train
+
 	except Exception as e:
 		print(f"Error loading or processing data: {e}")
 		return
 	
 	probabilities = model.predict(X)
 
-	y_prob = probabilities[:, 1]
-
-	loss = cross_entropy(y_true, y_prob)
+	y_true_one_hot = one_hot_encode(y_raw)
+	
+	loss = cross_entropy(y_true_one_hot, probabilities)
+	print(f"Loss: {loss:.4f}")
 
 	predictions = np.argmax(probabilities, axis=1)
 

utils.py
@@ -37,7 +37,7 @@ def relu(x):
 """
 	Derivative of ReLU activation function
 """
-def relu_prime(x: np.matrix) -> np.matrix:
+def relu_prime(x: np.ndarray) -> np.ndarray:
 	return (x > 0).astype(int)
 
 def softmax(x: np.ndarray) -> np.ndarray:
@@ -69,3 +69,12 @@ def cross_entropy_prime(y_true, y_pred):
 	epsilon = 1e-15
 	y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
 	return - (y_true / y_pred) + ((1 - y_true) / (1 - y_pred))
+
+def one_hot_encode(y):
+	encoded = np.zeros((len(y), 2))
+	for i, label in enumerate(y):
+		if label == 'M':
+			encoded[i] = [0, 1]
+		else:
+			encoded[i] = [1, 0]
+	return encoded